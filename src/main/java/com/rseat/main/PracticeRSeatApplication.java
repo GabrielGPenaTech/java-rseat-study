06f0bd506eb6582c26103c0004cffb5ea64666ea
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-21 22:04:10 -0300

    start project

---
+++ src/main/java/com/rseat/main/PracticeRSeatApplication.java
@@ -0,0 +1,13 @@
+package com.rseat.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeRSeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeRSeatApplication.class, args);
+	}
+
+}
